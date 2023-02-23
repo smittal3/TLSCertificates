77059b88b60625f06317e8b84c57c05071ae1b46
smittal3 <shubhammittal2003@gmail.com>
2023-02-23 01:09:19 -0500

    updated script

--- Scripts/getData.sh
+++ Scripts/getData.sh
@@ -7,12 +7,12 @@
 
 
 time=$(date -d "yesterday 13:00" '+%m_%d_%y')
-# curl -o ../Majestic/top1m/"${time}.csv" https://downloads.majestic.com/majestic_million.csv 
+curl -o ../Majestic/top1m/"${time}.csv" https://downloads.majestic.com/majestic_million.csv 
 
 
 # On mac
-cisco_date=$(date -v-1d '+%Y-%m-%d')
+# cisco_date=$(date -v-1d '+%Y-%m-%d')
 
-# cisco_date=$(date -d "yesterday 13:00" '+%Y-%m-%d')
+cisco_date=$(date -d "yesterday 13:00" '+%Y-%m-%d')
 curl -o ../Umbrella/top1m/"${time}.zip" http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m-"${cisco_date}".csv.zip
 cd ../Umbrella/top1m && unzip "${time}.zip" && mv -f top-1m.csv "${time}.csv" && rm -r -f "${time}.zip"
